ba8a04d68de4e470bee530a5e56556d3cfbfe7f6
Jason <limaeight@gmail.com>
2024-04-27 02:37:01 -0400

    initial solution for io

--- io/ShannonEntropy.java
+++ io/ShannonEntropy.java
@@ -1,7 +1,21 @@
-package io;
-
 public class ShannonEntropy {
     public static void main(String[] args) {
-        System.out.println("Hello in ShannonEntropy");
+        int m = Integer.parseInt(args[0]);
+        double h = 0;
+        int[] inputs = StdIn.readAllInts();
+        int inputLength = inputs.length;
+
+        for (int i = 1; i <= m; i++) {
+            int count = 0;
+            for (int item : inputs) {
+                if (item == i) {
+                    count++;
+                }
+            }
+            double probability = count / (double) inputLength;
+            h += -(probability * (Math.log(probability) / Math.log(2)) * probability);
+        }
+
+        System.out.printf("%.4f\n", h);
     }
 }
