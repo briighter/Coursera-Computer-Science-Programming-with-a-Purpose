9d0220b8d15280c50c2e01967379f4ce53fce4b0
Jason <limaeight@gmail.com>
2024-04-30 18:50:18 -0400

    Finish Shannon Entropy

--- io/ShannonEntropy.java
+++ io/ShannonEntropy.java
@@ -1,19 +1,24 @@
 public class ShannonEntropy {
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]);
-        double h = 0;
-        int[] inputs = StdIn.readAllInts();
-        int inputLength = inputs.length;
+        double h = 0.0;
+        int[] inputs = new int[m];
+        int totalCount = 0;
 
-        for (int i = 1; i <= m; i++) {
-            int count = 0;
-            for (int item : inputs) {
-                if (item == i) {
-                    count++;
-                }
+        // Read integers from standard input and count their frequencies
+        while (!StdIn.isEmpty()) {
+            int value = StdIn.readInt();
+            if (value >= 1 && value <= m) {
+                inputs[value - 1]++;
+                totalCount++;
             }
-            double probability = count / (double) inputLength;
-            h += -(probability * (Math.log(probability) / Math.log(2)) * probability);
+        }
+
+        for (int freq : inputs) {
+            if (freq > 0) {
+                double probability = (double) freq / totalCount;
+                h -= probability * (Math.log(probability) / Math.log(2));
+            }
         }
 
         System.out.printf("%.4f\n", h);
