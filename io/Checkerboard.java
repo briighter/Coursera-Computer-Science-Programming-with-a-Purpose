77007e4d4952fd4227c1df93f27132f61b0c34e2
Jason <limaeight@gmail.com>
2024-05-02 22:15:15 -0400

    Finished checkerboard

--- io/Checkerboard.java
+++ io/Checkerboard.java
@@ -10,7 +10,7 @@ public class Checkerboard {
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= n; j++) {
                 if ((i + j) % 2 != 0) StdDraw.setPenColor(StdDraw.GRAY);
-                else                  StdDraw.setPenColor(StdDraw.BLUE);
+                else StdDraw.setPenColor(StdDraw.BLUE);
                 StdDraw.filledSquare(i + 0.5, j + 0.5, 0.5);
             }
         }
